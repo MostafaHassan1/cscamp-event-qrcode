6912acc8a9782752738675faa84e6e35d07510dc
mostafa hassan <mostafahassan999888@gmail.com>
2021-03-31 10:34:07 +0200

    Added migration and model for participant

---
+++ database/migrations/2021_03_30_222054_create_participants_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone_number');
+            $table->string('email')->unique();
+            $table->string('department');
+            $table->integer('academic_year')->unsigned();
+            $table->string('vjudge');
+            $table->boolean('attendend')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
