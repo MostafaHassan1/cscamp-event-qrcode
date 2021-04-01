09aa0ac7f57bb51530f0242766150491cc6713d4
mostafa hassan <mostafahassan999888@gmail.com>
2021-04-01 06:16:09 +0200

    Changing QRCode to png

--- resources/views/mails/participantQR.blade.php
+++ resources/views/mails/participantQR.blade.php
@@ -10,11 +10,11 @@
 
 <body>
     <p>Hello {{$participant->name}},</p>
-    <br>
     <p>&nbsp; Thank you for participating in CS Camp event.</p>
     <p>Please <strong>Save this QR Code image</strong> and show it to the event organizer when you attend</p>
-    {!! QrCode::size(250)
-    ->generate(url("/participants/{$participant->id}/confirm/{$hash}")); !!}
+    <img src="{!!$message->embedData(QrCode::format('png')
+                        ->generate(url("/participants/{$participant->id}/confirm/{$hash}")) , 'QrCode.png'
+        , 'image/png' )!!}">
 </body>
 
 </html>
